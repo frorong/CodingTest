39d876435659ef69619dbf5aa486f852e36efdf3
이승제 <106712562+Lee-Seungje@users.noreply.github.com>
2023-06-15 08:15:47 +0900

    [unrated] Title: 배열 조각하기, Time: 0.18 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.js
@@ -0,0 +1,7 @@
+function solution(arr, query) {
+    let answer = [...arr];
+    query.forEach((v,i) => {
+        i % 2 === 0 ? answer.splice(query[i]+1, answer.length) : answer.splice(0, query[i]);
+    })
+    return answer;
+}

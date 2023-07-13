8bc4ea358ce0d89a66de43b30644eca1d0364c77
이승제 <106712562+Lee-Seungje@users.noreply.github.com>
2023-07-14 08:45:44 +0900

    [level 2] Title: 짝지어 제거하기, Time: 73.57 ms, Memory: 55.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,13 @@
+function solution(s)
+{
+    let arr = s.split('');
+    let a = [];
+    arr.forEach((v,i) => {
+        a.push(v);
+        if(v === a[a.length - 2]){
+            a.pop();
+            a.pop();
+        }
+    })
+    return a.length > 0 ? 0 : 1;
+}

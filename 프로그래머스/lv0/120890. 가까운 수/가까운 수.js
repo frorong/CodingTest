7c2353d04561950cb6bf859e27d827ededb3c3d8
이승제 <106712562+Lee-Seungje@users.noreply.github.com>
2023-08-28 14:31:24 +0900

    [level 0] Title: 가까운 수, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
@@ -0,0 +1,8 @@
+function solution(array, n) {
+    var answer = array[0];
+    array.forEach((v,i) => {
+        if(Math.abs(n - answer) === Math.abs(n - v)) answer = answer > v ? v : answer;
+        else answer = Math.abs(n - answer) > Math.abs(n - v) ? v : answer
+    });
+    return answer;
+}

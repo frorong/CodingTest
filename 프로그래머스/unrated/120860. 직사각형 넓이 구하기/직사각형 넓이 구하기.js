0c49425870e2ae77dfd9473606e598b1267c9bce
이승제 <106712562+Lee-Seungje@users.noreply.github.com>
2023-10-25 08:15:50 +0900

    [level unrated] Title: 직사각형 넓이 구하기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.js
@@ -0,0 +1,7 @@
+function solution(dots) {
+    let newDots = [];
+    const ul = dots.reduce((a, b) => a[0] > b[0] ? b : a);
+    const ur = dots.reduce((a, b) => a[0] < b[0] ? b : a);
+    const dl = dots.reduce((a, b) => a[1] > b[1] ? a : b);
+    return (ur[0] - ul[0]) * (dl[1] - ul[1]);
+}

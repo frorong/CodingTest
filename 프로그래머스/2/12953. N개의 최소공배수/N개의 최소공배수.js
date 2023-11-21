094e2c7ec8fc723bf5267d8a336318a9a2faa881
이승제 <106712562+Lee-Seungje@users.noreply.github.com>
2023-11-22 08:04:23 +0900

    [level 2] Title: N개의 최소공배수, Time: 246.39 ms, Memory: 36.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -0,0 +1,7 @@
+function solution(arr) {
+    for(let i = 1; ; i++){
+        let isInsu = true;
+        arr.forEach((v) => {if(i % v !== 0) isInsu = false});
+        if(isInsu) return i
+    }
+}

099b8fea63d1bc83eb91abe68a2a3009f76259b6
이승제 <106712562+frorong@users.noreply.github.com>
2023-12-28 13:48:54 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 142.79 ms, Memory: 56.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,9 +1,5 @@
 function solution(participant, completion) {
     participant.sort();
     completion.sort();
-    for(var i = 0; i < participant.length; i++){
-        if(participant[i] !== completion[i]){
-            return participant[i];
-        }
-    }
+    for(var i = 0; i < participant.length; i++) if(participant[i] !== completion[i]) return participant[i];
 }

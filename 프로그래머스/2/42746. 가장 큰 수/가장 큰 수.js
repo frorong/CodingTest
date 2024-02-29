bf68fe97b822471cc13b13f8383f6f26991674a2
이승제 <106712562+frorong@users.noreply.github.com>
2024-02-29 12:11:30 +0900

    [level 2] Title: 가장 큰 수, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -0,0 +1,10 @@
+function solution(numbers) {
+    numbers = numbers.sort((a,b) => {
+        const [na,nb] = [String(a), String(b)];
+        const ab = na + nb;
+        const ba = nb + na;
+        return ba - ab;
+    }).join('');
+    
+    return numbers[0] === '0' ? '0' : numbers;
+}

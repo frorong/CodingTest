5861547225968b2f02abca10d9c109bf716210d8
이승제 <106712562+frorong@users.noreply.github.com>
2024-03-28 15:33:33 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 12.81 ms, Memory: 45 MB -BaekjoonHub

---
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -0,0 +1,29 @@
+function solution(queue1, queue2) {
+    let answer = 0;
+    
+    let idx1 = 0;
+    let idx2 = 0;
+    let cnt1 = queue1.reduce((a,b)=> a+b, 0);
+    let cnt2 = queue2.reduce((a,b)=> a+b, 0);
+    
+    const maximam = queue1.length + queue2.length * 2;
+
+    while(answer < maximam) {
+        if(cnt1 > cnt2){
+            const target = queue1[idx1++];
+            cnt1 -= target;
+            cnt2 += target;
+            queue2.push(target);
+        }
+        else if(cnt1 < cnt2) {
+            const target = queue2[idx2++];
+            cnt1 += target;
+            cnt2 -= target;
+            queue1.push(target);
+        }
+        else return answer;
+        answer++;
+    }
+
+    return -1;
+}
